620b7fe09ea10864292382e66b7c419d7f198a2c
James Sandy <jamessandy95@gmail.com>
2024-12-10 15:31:29 +0000

    update ngrok again

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // Todo, can we get this working dynamically, favouring local if it is running?
 // const backend_url = "http://127.0.0.1:7878";
-const backend_url = "https://d287-192-91-191-20.ngrok-free.app";
+const backend_url = "https://c003-192-91-191-20.ngrok-free.app";
 //
